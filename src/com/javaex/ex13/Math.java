0d9faa54584c072d552827826e41f1c8117c2787
aassdd236 <aassdd236@naver.com>
2024-01-05 13:00:47 +0900

    01/05

---
+++ src/com/javaex/ex13/Math.java
@@ -0,0 +1,29 @@
+package com.javaex.ex13;
+
+public class Math {
+
+	//필드
+	
+	//생성자
+	
+	//메소드(get, set)
+	
+	//메소드 일반
+	public static int plus(int a, int b) {
+		return a+b;
+	}
+	public static double plus(double a, int b) {
+		return a+b;
+	}
+	public static double plus(int a, double b) {
+		return a+b;
+	}
+	public static double plus(double a, double b) {
+		return a+b;
+	}
+	public static double circleArea(int radius) {
+		
+		return 3.14*radius*radius;
+		
+	}
+}
---
+++ src/com/javaex/ex13/MathApp.java
@@ -0,0 +1,13 @@
+package com.javaex.ex13;
+
+public class MathApp {
+
+	public static void main(String[] args) {
+		
+		Math math=new Math();
+		System.out.println(Math.plus(2, 2));
+
+		System.out.println(Math.circleArea(5));
+	}
+
+}
