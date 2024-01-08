eeca464f17af2cb30ad8dae8ba0393de3c1114ad
aassdd236 <aassdd236@naver.com>
2024-01-08 15:47:11 +0900

    01/08

--- src/com/javaex/ex16/ColorPoint.java
+++ src/com/javaex/ex16/ColorPoint.java
@@ -6,13 +6,23 @@ public class ColorPoint extends Point {
 	private String color;
 
 	//생성자
+	
 	public ColorPoint(String color) {
+		super();
 		this.color = color;
 	}
 	
+	public ColorPoint() {
+		super();
+		System.out.println("ColorPoint()");
+	}
+
 	public ColorPoint(int x, int y, String color) {
-		super(x, y);
+		super();
+		super.setX(x);
+		super.setY(y);
 		this.color = color;
+		System.out.println("ColorPoint(3)");
 	}
 	
 	//메소드
@@ -27,5 +37,9 @@ public class ColorPoint extends Point {
 	@Override
 	public String toString() {
 		return "ColorPoint [color=" + color + "]";
+	}
+	
+	public void print() {
+		System.out.println("점 x = "+super.x+" 점 y = "+super.getY()+" 색깔: "+color);
 	}
 }
--- src/com/javaex/ex16/PointApp.java
+++ src/com/javaex/ex16/PointApp.java
@@ -5,6 +5,7 @@ public class PointApp {
 	public static void main(String[] args) {
 		
 		Point p = new Point(4,4);
+		p.x=3;
 		
 		ColorPoint  cp1 = new ColorPoint("red");
 		ColorPoint  cp2 = new ColorPoint(10, 10, "blue");
