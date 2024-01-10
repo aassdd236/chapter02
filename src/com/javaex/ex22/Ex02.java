8a0a197d58c0647ff909d13f08caea70df76b9d6
aassdd236 <aassdd236@naver.com>
2024-01-10 18:03:32 +0900

    01/10

--- src/com/javaex/ex22/Ex02.java
+++ src/com/javaex/ex22/Ex02.java
@@ -3,8 +3,17 @@ package com.javaex.ex22;
 public class Ex02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] iArr = new int[] {3, 6, 9};
 
+		try {
+			for (int i = 0; i < iArr.length + 1; i++) {
+				System.out.print(iArr[i] + " ");
+			}
+		}catch(ArrayIndexOutOfBoundsException arr) {
+			System.out.println();
+			System.out.println(arr.toString());
+			System.out.println("배열의 범위를 벗어나 강제종료 합니다.");
+		}
 	}
 
 }
